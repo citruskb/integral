831f9916247b28c176f2a8f053b7f5ef7887ff4e
citruskb <140352993+citruskb@users.noreply.github.com>
2026-06-16 19:05:55 -0400

    Add math.Sign()

--- lua/integral/libraries/sh_extend_math.lua
+++ lua/integral/libraries/sh_extend_math.lua
@@ -2,3 +2,4 @@ local math_PI = math.PI
 
 function math.Remainder(num) return num - math.Truncate(num) end
 function math.Ang(rad) return rad * 180 / math_PI end
+function math.Sign(num) return num < 0 and -1 or num == 0 and 0 or num > 0 and 1 end

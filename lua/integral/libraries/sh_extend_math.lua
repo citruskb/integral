2c2cb7bc76c865ee9f9ee00591bbca007d36d564
citruskb <140352993+citruskb@users.noreply.github.com>
2026-07-05 13:52:41 -0400

    Make sure Math.Ang() returns valid degrees

--- lua/integral/libraries/sh_extend_math.lua
+++ lua/integral/libraries/sh_extend_math.lua
@@ -1,5 +1,5 @@
 local math_PI = math.PI
 
 function math.Remainder(num) return num - math.Truncate(num) end
-function math.Ang(rad) return rad * 180 / math_PI end
+function math.Ang(rad) return (rad * 180 / math_PI) % 360 end
 function math.Sign(num) return num < 0 and -1 or num == 0 and 0 or num > 0 and 1 end
